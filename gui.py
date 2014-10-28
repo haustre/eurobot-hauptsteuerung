e1625a838b52660a6b3e0fcc5edc09b87cb0b6ff
wursm1 <wursm1@bfh.ch>
2014-10-28 15:01:36 +0100

    can_traffic -> kern -> gui funktioniert (ohne Kodierung)

--- gui.py
+++ gui.py
@@ -8,7 +8,7 @@ def main():
     port = 42233        #Test
     data = {'message':'hello world!', 'test': 123.4}
     tcp = Client(host, port)
-    tcp.write(data)
+    #tcp.write(data)
     while 1:
         data = tcp.read()
         if data:
--- kern.py
+++ kern.py
@@ -7,12 +7,12 @@ import time
 
 def main():
     can_id, can_mask = 0x600, 0x600
-    s_debug = Can(can_id, can_mask)
+    s_debug = Can(can_id, can_mask, 'vcan0')
     tcp = Server()
     while True:
         data = s_debug.receive()
         if data:
-            tcp.write(data)
+            tcp.write(str(data))
             print(data)
         time.sleep(0.01)
 
