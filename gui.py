8ad44660c0136f933832509d20726a5e9b88ea5d
wursm1 <wursm1@bfh.ch>
2014-10-28 21:38:45 +0100

    -

--- gui.py
+++ gui.py
@@ -4,23 +4,29 @@ import time
 from ethernet import Client
 from PyQt4 import QtGui
 import sys
+import threading
+
 
 class Table(QtGui.QTableWidget):
     def __init__(self, header):
-        super().__init__(1, len(header))
+        super().__init__(5, len(header))
+        self.header = header
         self.setHorizontalHeaderLabels(header)
         self.verticalHeader().setVisible(False)
         #self.resizeColumnsToContents()
         #self.resizeRowsToContents()
+        #self.test()
+
+    def test(self):
+        for i in range(100):
+            self.setRowCount(self.rowCount() + 1)
+            for ii in range(len(self.header)):
+                newitem = QtGui.QTableWidgetItem(str(i+0.2*ii))
+                self.setItem(i, ii, newitem)
+                time.sleep(0.1)
 
 
-def main(args):
-    app = QtGui.QApplication(args)
-    header = ['Time', 'Source', 'Type']
-    can_table = Table(header)
-    can_table.show()
-    sys.exit(app.exec_())
-
+def test():
     host = 'localhost'  #Test
     port = 42233        #Test
     data = {'message':'hello world!', 'test': 123.4}
@@ -32,5 +38,27 @@ def main(args):
             print(data)
             pass
         time.sleep(0.1)
+
+
+def test2():
+    while 1:
+        print("Hallo")
+        time.sleep(2)
+
+
+def main(args):
+    app = QtGui.QApplication(args)
+    header = ['Time', 'Source', 'Type']
+    can_table = Table(header)
+    can_table.show()
+
+    t = threading.Thread(target=test2)
+    t.setDaemon(1)
+    t.start()
+
+
+    sys.exit(app.exec_())
+
+
 
 main(sys.argv)
