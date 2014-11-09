5116a7bc8906c74a87883b4ecf2d973445527172
wursm1 <wursm1@bfh.ch>
2014-11-09 22:47:45 +0100

    EditHost verschoben

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ import time
 import sys
 
 from PyQt4 import QtGui
-from gui.qt_table import Table
+import gui.qt_table
 from ethernet import Client
 import threading
 import datetime
@@ -16,11 +16,11 @@ class CanWindow(QtGui.QWidget):
         self.threads = []
         self.connected = False
         header = ['Time', 'Source', 'Type', 'Value']
-        self.can_table = Table(header)
+        self.can_table = gui.qt_table.Table(header)
         self.init_ui()
 
     def init_ui(self):
-        edit_host = EditHost(self)
+        edit_host = gui.qt_table.EditHost(self)
         vbox = QtGui.QVBoxLayout()
         vbox.addWidget(self.can_table)
         vbox.addWidget(edit_host)
@@ -47,32 +47,6 @@ class CanWindow(QtGui.QWidget):
                 time.sleep(0.5)
         else:
             return
-
-
-class EditHost(QtGui.QWidget):
-    def __init__(self, parent):
-        super().__init__()
-        self.parent = parent
-        host_label = QtGui.QLabel('Host:')
-        self.host_line = QtGui.QLineEdit('localhost')
-        port_label = QtGui.QLabel('Port:')
-        self.port_line = QtGui.QLineEdit('42233')
-        host_button = QtGui.QPushButton('Connect')
-        host_button.clicked.connect(self.connect_host)
-
-        grid = QtGui.QGridLayout()
-        grid.addWidget(host_label, 0, 0)
-        grid.addWidget(self.host_line, 0, 1)
-        grid.addWidget(port_label, 1, 0)
-        grid.addWidget(self.port_line, 1, 1)
-        grid.addWidget(host_button, 1, 2)
-        self.setLayout(grid)
-
-    def connect_host(self):
-        if self.parent.connected is False:
-            self.parent.connect_host(self.host_line.text(), self.port_line.text())
-        else:
-            print("Already connected")
 
 
 def main(args):
--- gui/qt_table.py
+++ gui/qt_table.py
@@ -24,3 +24,29 @@ class Table(QtGui.QTableWidget):
         slide_bar.setValue(slide_bar.maximum())
         self.showColumn(row_count)
         print(row_count)
+
+
+class EditHost(QtGui.QWidget):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        host_label = QtGui.QLabel('Host:')
+        self.host_line = QtGui.QLineEdit('localhost')
+        port_label = QtGui.QLabel('Port:')
+        self.port_line = QtGui.QLineEdit('42233')
+        host_button = QtGui.QPushButton('Connect')
+        host_button.clicked.connect(self.connect_host)
+
+        grid = QtGui.QGridLayout()
+        grid.addWidget(host_label, 0, 0)
+        grid.addWidget(self.host_line, 0, 1)
+        grid.addWidget(port_label, 1, 0)
+        grid.addWidget(self.port_line, 1, 1)
+        grid.addWidget(host_button, 1, 2)
+        self.setLayout(grid)
+
+    def connect_host(self):
+        if self.parent.connected is False:
+            self.parent.connect_host(self.host_line.text(), self.port_line.text())
+        else:
+            print("Already connected")
