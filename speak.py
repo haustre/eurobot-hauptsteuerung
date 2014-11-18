07d154096502268aee52a3c6090226a92a421f0c
wursm1 <wursm1@bfh.ch>
2014-11-18 11:14:17 +0100

    -

--- speak.py
+++ speak.py
@@ -6,4 +6,4 @@ import subprocess
 def speak(text):
     language = 'en'
     speed = '175'  # Speed in words per minute, 80 to 450, default is 175
-    command1 = subprocess.Popen(['espeak', '-v', language, '-s', speed, str(text)])
+    command = subprocess.Popen(['espeak', '-v', language, '-s', speed, str(text)])
