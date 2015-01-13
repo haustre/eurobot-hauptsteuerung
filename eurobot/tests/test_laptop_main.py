1cc45a525e62f9837ae0922d4d1e6d506760fec1
wursm1 <wursm1@bfh.ch>
2015-01-13 11:09:48 +0100

    test_laptop starts the gui

--- eurobot/tests/test_laptop_main.py
+++ eurobot/tests/test_laptop_main.py
@@ -2,28 +2,33 @@ __author__ = 'Würsch Marcel'
 __license__ = "GPLv3"
 
 import unittest
-import sys
 from PyQt4.QtGui import QApplication
 from PyQt4.QtTest import QTest
 from PyQt4.QtCore import Qt
 import laptop_main
+from eurobot.libraries import can
 
-import time
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        app = QApplication(['/home/mw/PycharmProjects/Eurobot/eurobot/laptop_main.py'])
-        self.can_window = laptop_main.CanWindow()
-        self.can_window.show()
-        sys.exit(app.exec_())
+        can_connection = can.Can('vcan0', can.MsgSender.Debugging)
+
+        # Initialization
+        self.app = QApplication([])
+        # Main Window creation.
+        self.MainWin = laptop_main.CanWindow()
+        # EXECUTION
+        self.MainWin.show()
+        QTest.qWaitForWindowShown(self.MainWin)
+        # Don't call exec or your qtest commands won't reach widgets.
 
     def tearDown(self):
-        del self.can_window
+        del self.MainWin
 
     def test_something(self):
-        ok_widget = self.can_window.edit_host.host_button(self.can_window.edit_host)
-        #okWidget = self.form.ui.buttonBox.button(self.form.ui.buttonBox.Ok)
-        QTest.mouseClick(ok_widget, Qt.LeftButton)
+        self.MainWin.edit_host.host_line.clear()
+        QTest.keyClicks(self.MainWin.edit_host.host_line, "localhost")
+        QTest.mouseClick(self.MainWin.edit_host.host_button, Qt.LeftButton, delay=1)
 
 
 
